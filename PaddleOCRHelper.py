59506c816f0f3283ff9d6c709fafb08bd7d01da2
icear <icear@users.noreply.github.com>
2022-05-27 15:03:15 +0800

    add OCR helper

--- PaddleOCRHelper.py
+++ PaddleOCRHelper.py
@@ -31,27 +31,29 @@ ocr = PaddleOCR(use_angle_cls=False, lang="ch")  # 初始化模型
 logging.info("start read from adb")
 # 获取图片
 controller = ADBController()
-image = np.frombuffer(controller.get_device_screen_picture(), dtype="int8")
-# cv_image = cv.cvtColor(cv.imdecode(image, cv.IMREAD_COLOR), cv.COLOR_BGRA2RGB)
-cv_image = cv.imdecode(image, cv.IMREAD_COLOR)
 
-logging.info("start ocr")
-# 开始识别
-result = ocr.ocr(img=cv_image, cls=False)
+while True:
+    image = np.frombuffer(controller.get_device_screen_picture(), dtype="int8")
+    # cv_image = cv.cvtColor(cv.imdecode(image, cv.IMREAD_COLOR), cv.COLOR_BGRA2RGB)
+    cv_image = cv.imdecode(image, cv.IMREAD_COLOR)
 
-logging.info("process ocr result")
+    logging.info("start ocr")
+    # 开始识别
+    result = ocr.ocr(img=cv_image, cls=False)
 
-logging.info(cv_image.shape)
-# cv_image = cv.imdecode(image, cv.IMREAD_COLOR)
-boxes = [line[0] for line in result]
-txts = [line[1][0] for line in result]
-scores = [line[1][1] for line in result]
-# im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
-for index, box in enumerate(boxes):
-    logging.info((box[0][0], box[0][1]))
-    logging.info((box[2][0], box[2][1]))
-    logging.info(txts[index])
-    cv.rectangle(img=cv_image, pt1=(int(box[0][0]), int(box[0][1])), pt2=(int(box[2][0]), int(box[2][1])),
-                 color=(255, 0, 0), thickness=3)
-cv.imshow('image_original', cv_image)
-cv.waitKey(0)
+    logging.info("process ocr result")
+
+    logging.info(cv_image.shape)
+    # cv_image = cv.imdecode(image, cv.IMREAD_COLOR)
+    boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    scores = [line[1][1] for line in result]
+    # im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
+    for index, box in enumerate(boxes):
+        logging.info((box[0][0], box[0][1]))
+        logging.info((box[2][0], box[2][1]))
+        logging.info(txts[index])
+        cv.rectangle(img=cv_image, pt1=(int(box[0][0]), int(box[0][1])), pt2=(int(box[2][0]), int(box[2][1])),
+                     color=(255, 0, 0), thickness=3)
+    cv.imshow('image_original', cv_image)
+    cv.waitKey(0)
